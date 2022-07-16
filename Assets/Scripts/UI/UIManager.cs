4a7fbb87f71fce573ea60ecbf569eb0b92581341
dadames <dadames999@gmail.com>
2022-07-16 15:06:23 -0600

    Renamed InGameUI to UIManager

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
-public class InGameMenu : MonoBehaviour
+public class UIManager : MonoBehaviour
 {
     public GameObject menus;
     public GameObject menu;
