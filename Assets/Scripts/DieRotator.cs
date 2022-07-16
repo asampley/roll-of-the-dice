da3f30bc20cec7658995709535fe300b96c33935
Alex Sampley <alex.sampley@gmail.com>
2022-07-15 19:58:51 -0600

    Fixed count not being used when doing rotations

--- Assets/Scripts/DieRotator.cs
+++ Assets/Scripts/DieRotator.cs
@@ -1,10 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DieRotator : MonoBehaviour {
-    private float rotateTime = 1.0f;
-
     private Quaternion current;
     private Quaternion target;
     private float startTime;
@@ -17,34 +16,42 @@ public class DieRotator : MonoBehaviour {
     void Start() {
         this.current = this.transform.rotation;
         this.target = this.transform.rotation;
-        this.duration = 0.5f;
 
         this.localUp = this.current * Vector3.up;
         this.localForward = this.current * Vector3.forward;
         this.localRight = this.current * Vector3.right;
     }
 
-    public void RotateX(int count) {
+    private void Rotate(Quaternion rotation, int count) {
+        if (count < 0) {
+            rotation = Quaternion.Inverse(rotation);
+        }
 
-        var targetInverse = Quaternion.Inverse(target);
-        this.target *= Quaternion.FromToRotation(targetInverse * localRight, targetInverse * localForward);
+        for (int i = 0; i < Math.Abs(count); ++i) {
+            target *= rotation;
+        }
 
         this.current = this.transform.rotation;
         this.startTime = Time.fixedTime;
+    }
+
+    public void RotateX(int count) {
+        var targetInverse = Quaternion.Inverse(target);
+        var rotation = Quaternion.FromToRotation(targetInverse * localForward, targetInverse * localRight);
+
+        Rotate(rotation, count);
     }
 
     public void RotateY(int count) {
-
         var targetInverse = Quaternion.Inverse(target);
-        this.target *= Quaternion.FromToRotation(targetInverse * localUp, targetInverse * localForward);
+        var rotation = Quaternion.FromToRotation(targetInverse * localForward, targetInverse * localUp);
 
-        this.current = this.transform.rotation;
-        this.startTime = Time.fixedTime;
+        Rotate(rotation, count);
     }
 
     public void Update() {
-        if (Time.fixedTime < startTime + duration) {
-            this.transform.rotation = Quaternion.Slerp(current, target, (Time.fixedTime - startTime) / duration);
+        if (Time.fixedTime < startTime + Globals.MOVEMENT_TIME) {
+            this.transform.rotation = Quaternion.Slerp(current, target, (Time.fixedTime - startTime) / Globals.MOVEMENT_TIME);
         } else {
             this.transform.rotation = target;
         }
