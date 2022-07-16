edeeaaf56f0aaf1a394b8d6cf61c506681ea9657
dadames <dadames999@gmail.com>
2022-07-16 10:32:12 -0600

    Added main menu scene and script

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject levelSelect;
+    public GameObject instructions;
+    public GameObject options;
+
+    public void PlayGame()
+    {
+        mainMenu.SetActive(false);
+        levelSelect.SetActive(true);
+    }
+
+    public void Instructions()
+    {
+        mainMenu.SetActive(false);
+        instructions.SetActive(true);
+    }
+
+    public void Options()
+    {
+        mainMenu.SetActive(false);
+        options.SetActive(true);
+    }
+
+    public void Back()
+    {        
+        levelSelect.SetActive(false);
+        instructions.SetActive(false);
+        options.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+}
