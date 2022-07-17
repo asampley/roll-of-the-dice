6e27456bf0ab6384f36290d180a1141983e2724d
Alex Sampley <alex.sampley@gmail.com>
2022-07-16 21:50:02 -0600

    Adde level 3 and 4 to enum

--- Assets/Scripts/UI/Loader.cs
+++ Assets/Scripts/UI/Loader.cs
@@ -12,6 +12,8 @@ public static class Loader
         MainMenu = 0,
         Level1 = 1,
         Level2 = 2,
+        Level3 = 3,
+        Level4 = 4,
     }
 
     private static Action onLoaderCallback;
